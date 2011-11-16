2b5a9ad3fe1bb4582117f91426f104df83aa7d47
Mario Garcia Valdez <mariosky@gmail.com>
2011-11-16 15:24:30 -0800

    Multiple Populations
    Added support for multiple populations on the same server.

--- EvoSpace.py
+++ EvoSpace.py
@@ -3,7 +3,6 @@ __author__ = 'mario'
 HOST = '127.0.0.1'
 PORT = 6379
 DB = 0
-SERVER_LIST = ['protoboard.com/population']
 
 import redis, time , json, random
 
@@ -11,17 +10,18 @@ r = redis.Redis(host=HOST, port=PORT, db=DB)
 
 class Individual:
     def __init__(self, key = None , fitness = {}  , chromosome  = None , from_dict = None):
-        ## Debe de existir o Key o from_dict
+        ## Se puede inicializar desde un diccionario
         if from_dict:
             self.key = from_dict['id']
             self.fitness = from_dict['fitness']
             self.chromosome = from_dict['chromosome']
         else:
+        ## Se tiene minimo la llave
             self.key = key
             self.fitness = fitness
             self.chromosome = chromosome
 
-    def put(self, population= 'population'):
+    def put(self, population):
         pipe = r.pipeline()
         if pipe.sadd( population, self.key ):
             pipe.hset( self.key ,'fitness' , self.fitness )
@@ -34,6 +34,8 @@ class Individual:
         
     def get(self, as_dict = False):
         if r.exists( self.key ):
+            #Se evalua el texto almacenado en Redis
+            #Un diccionario para la aptitud y una lista para el cromosma
             self.fitness =  eval(r.hget(self.key ,'fitness'))
             self.chromosome = eval (r.hget( self.key ,'chromosome'))
         if as_dict:
@@ -50,29 +52,41 @@ class Individual:
 
 
 class Population:
-    def __init__(self, initial_size = 10000, flush = True ):
+    def __init__(self, name = "population" ):
+        self.name = name
+        self.sample_counter = self.name+':sample_count'
+        self.individual_counter = self.name+':individual_count'
+        self.sample_queue = self.name+":sample_queue"
+
+    def individual_next_key(self):
+        key = r.incr(self.sample_counter)
+        return self.name+":individual:%s" % key
+
+    def initialize(self, initial_size = 1000):
         self.initial_size = initial_size
-        self.flush = flush
-        if self.flush:
-            r.flushall()
-            r.setnx('sample',0)
-            r.setnx('individual',0)
+        r.flushall()
+        r.setnx(self.sample_counter,0)
+        r.setnx(self.individual_counter,0)
 
     def get_sample(self, size):
-        sample_id = r.incr('sample')
-        sample = [r.spop('population') for i in range(size)]
-        r.sadd("sample:%s" % sample_id, *sample)
-        result =  {'sample_id':"sample:%s" % sample_id ,
-                   'sample':   [Individual(key).get(as_dict=True) for key in sample],
-                   'server':SERVER_LIST }
+        sample_id = r.incr(self.sample_counter)
+        sample = [r.spop(self.name) for i in range(size)]
+        r.sadd(self.name+":sample:%s" % sample_id, *sample)
+        r.lpush(self.sample_queue, self.name+":sample:%s" % sample_id)
+        result =  {'sample_id': self.name+":sample:%s" % sample_id ,
+                   'sample':   [Individual(key).get(as_dict=True) for key in sample]}
         return json.dumps(result)
 
     def read_sample(self, size):
-        sample_id = r.incr('sample')
-        sample = [r.srandmember('population') for i in range(size)]
-        return {'sample_id':"sample:%s" % sample_id ,
-                'sample':   [Individual(key).get(as_dict=True) for key in sample],
-                'server':SERVER_LIST }
+        sample_id = r.incr(self.sample_counter)
+        sample = [r.spop(self.name) for i in range(size)]
+        result =  {'sample_id': self.name+":sample:%s" % sample_id ,
+                   'sample':   [Individual(key).get(as_dict=True) for key in sample]}
+        return json.dumps(result)
+
+    def put_individual(self, key = None , fitness = {}  , chromosome  = None , from_dict = None ):
+        ind = Individual( key, fitness = fitness , chromosome  = chromosome, from_dict = from_dict)
+        ind.put(self.name)
 
     def put_sample(self,sample):
         if type(sample) == 'str':
@@ -81,75 +95,51 @@ class Population:
         if r.exists(sample['sample_id']):
             for member in sample['sample']:
                 if member['id'] is None:
-                    member['id'] = "individual:%s" % r.incr('individual')
-                new = Individual( member['id'], fitness = member['fitness'] , chromosome  = member['chromosome'])
-                new.put()
+                    member['id'] = self.name+":individual:%s" % r.incr(self.individual_counter)
+                put_individual( member['id'], fitness = member['fitness'] , chromosome  = member['chromosome'])
             r.delete(sample['sample_id'])
-
+            r.lrem(self.sample_queue,sample['sample_id'])
 
     def respawn_sample(self, sample):
         members = r.smembers(sample)
-        r.sadd('population',*members)
+        r.sadd(self.name, *members)
         r.delete(sample)
+        r.lrem(self.sample_queue,sample,1)
 
-PALABRA = "Hello World"
-population = Population()
-#Se incializa, ya que esto no es responsabilidad de la poblacion
-for i in range(population.initial_size):
-    key = r.incr('individual')
-    chrome = [random.randint(1, 255) for i in range(len(PALABRA))]
-    new = Individual("individual:%s" % key, fitness={"DefaultContext":0.0 }, chromosome  = chrome )
-    new.put()
+    def respawn_ratio(self, ratio = .2):
+        until_sample  = int(r.llen(self.sample_queue)*ratio)
+        for i in range(until_sample):
+            respawn_sample( r.lpop(self.sample_queue))
+
+
+    def respawn(self, n = 1):
+        current_size = r.llen(self.sample_queue)
+        if n > current_size:
+            for i in range(n):
+                respawn_sample( r.lpop(self.sample_queue))
+        else:
+            for i in range(current_size):
+                respawn_sample( r.lpop(self.sample_queue))
+
+
+
+def init_population(population):
+    PALABRA = "Hello World"
+    population.initialize()
+    for i in range(population.initial_size):
+        chrome = [random.randint(1, 255) for i in range(len(PALABRA))]
+        key = population.individual_next_key()
+        population.put_individual(key, fitness={"DefaultContext":0.0 }, chromosome  = chrome )
+
+
+population = Population("population")
+init_population(population)
 
 #clock_start = time.clock()
 #time_start = time.time()
-#
-#
-#
-##Se crea la poblacion y se borran los datos anteriores
-#population = Population()
-##Para el caso de Hello World
-#PALABRA = "Hello World"
-#
-##Se incializa, ya que esto no es responsabilidad de la poblacion
-#for i in range(population.initial_size):
-#    key = r.incr('chromosome')
-#    chrome = [random.randint(1, 255) for i in range(len(PALABRA))]
-#    new = Chromosome("chromosome:%s" % key, chromosome  = chrome )
-#    new.put()
-#
-#
+
 #time_end = time.time()
 #clock_end = time.clock()
-#
-#print 'TOTAL CLOCK', clock_end-clock_start
-#print 'TOTAL TIME', time_end-time_start
-#
-#print '*'
-#
-#clock_start = time.clock()
-#time_start = time.time()
-#
-##for i in range(100):
-#sample = population.get_sample(3)
-#print json.dumps(sample)
-#
-#print sample["sample"][0]["chromosome"]
-#sample["sample"][0]["chromosome"] = [60, 190, 223, 121, 24, 67, 139, 68, 252, 217, 172]
-#sample["sample"][0]["id"] = None
-#sample["sample"][1]["id"] = None
-#sample["sample"][0]["fitness"]= 0.5
-#print sample["sample_id"]
-#
-#print sample["sample"][0]
-#population.read_sample(3)
-#population.put_sample(sample)
-##population.respawn(sample_id)
-#
-#
-#time_end = time.time()
-#clock_end = time.clock()
-#
 #
 #
 #sample1 = {'sample_id':'sample:69' ,
--- cherryserver.py
+++ cherryserver.py
@@ -1,11 +1,16 @@
-# -*- coding: utf-8 -*-
+#-*- coding: utf-8 -*-
 __author__ = 'mario'
 
 import cherrypy
-import os
+import os, json
 import EvoSpace
+
+
+
+EvoSpace.init_population()
 # a foo.html file will contain our Dojo code performing the XHR request
 # and that's all the following config directive is doing
+
 current_dir = os.getcwd()
 config = {'/static' :
     {
@@ -17,15 +22,10 @@ config = {'/static' :
 class Content:
     @cherrypy.expose
     def EvoSpace(self):
-    #############################################################
-    # for sheer simplicity, this example does not use a json lib.
-    # for anything more sophisticated than this example,
-    # get a good json library from http://json.org
-    ############################################################
     # read the raw POST data
         rawPost = cherrypy.request.body.read( )
     # cast to object
-        obj = eval(rawPost) #MAJOR security hole! you’ve been warned...
+        obj = json.loads(rawPost)
     # process the data
         if obj["method"] == "getSample":
             result = EvoSpace.population.get_sample(obj["params"][0])
@@ -34,14 +34,13 @@ class Content:
     # return a json response
         cherrypy.response.headers['Content-Type']= 'text/json-comment-filtered'
         return str({"result" : result})
-    # start up the web server and have it listen on 8080
 
     @cherrypy.expose
     def index(self):
         return "Servidor Funcionando"
 
 
-
+# start up the web server and have it listen on 8088
 cherrypy.config.update({'server.socket_host': '127.0.0.1',
                         'server.socket_port': 8088
                        })
